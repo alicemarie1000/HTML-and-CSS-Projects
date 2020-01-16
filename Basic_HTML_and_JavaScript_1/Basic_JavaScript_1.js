c870b429fca08329f8589552eac1b546a1f8928d
alicemarie1000 <alicemarie1000@users.noreply.github.com>
2020-01-16 17:35:17 -0500

    Function Assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World! I am an alert box!");
+
+   	function My_First_Function() {
+	   var String = "kiss me, I'm Irish!";
+	   var result = String.font.color("green");
+	   document.getElementById("Irish").innerHTML = result;
+      }
